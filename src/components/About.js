1cc61c44463115dc40ae002e80e533bc52be380b
Denny-svg <code_sakata@sakataas>
2024-01-30 15:38:22 +0300

    Done assignment

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,16 @@
-import React from "react";
-import { image } from "../data/data";
+import React from 'react';
+
+
+import { image } from "../data/data.js";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>The best developer in the City.</p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
 }
 
 export default About;
